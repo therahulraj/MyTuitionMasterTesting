c9a3209f00cae338a0370bd8e35bf4b2156a8eec
rahulraj16mar <31818361+rahulraj16mar@users.noreply.github.com>
2018-09-16 03:31:38 +0530

    modified

--- public/js/basicjs.js
+++ public/js/basicjs.js
@@ -1,27 +1,36 @@
 $(document).ready(function() {
-$('.svg-hamburger').on('click', function () {
-    $('.navbar-content').toggleClass('navbar-content-slide');
-    $('body').toggleClass('stop-scrolling');
-  });
-  $('.body2-row3-button1 button').on('click', function () {
-    $('.body2-row2-extra-chat-row6 button').click();
-  });
+  var $win = $(window);
+  $('.svg-hamburger').on('click', function () {
+      $('.navbar-content').toggleClass('navbar-content-slide');
+      $('body').toggleClass('stop-scrolling');
+    });
+    $('.body2-row3-button1 button').on('click', function () {
+      $('.body2-row2-extra-chat-row6 button').click();
+    });
+  $win.on('scroll', function () {
+      var top = $win.scrollTop();
+      if(top > $('#section-6').offset().top - 200) {
+     $('.chat-cover').css('display', 'none');
+   } else {
+     $('.chat-cover').css('display', 'block');
+   }
+  })
   if (window.innerWidth < 800) {
+      $('.body3').parallax({imageSrc: '../images/hanscopy1.jpg'});
 $('.mobile-symbol').on('click', function () {
       $('.chat-cover').addClass('mobile-symbol-slide');
       $('.chat').addClass('chat-mobile-symbol-slide');
       $('.chat-arrow').addClass('rotate-arrow');
-      $('body').addClass('stop-scrolling');
     });
     $('.chat-row1').on('click', function () {
       $('.chat-cover').removeClass('mobile-symbol-slide');
         $('.chat').removeClass('chat-mobile-symbol-slide');
         $('.chat-arrow').removeClass('rotate-arrow');
-              $('body').removeClass('stop-scrolling');
     });
 
 } else if(window.innerWidth > 800) {
-$('.chat-row1').on('click', function () {
+  $('.body3').parallax({imageSrc: '../images/hans.jpg'});
+  $('.chat-row1').on('click', function () {
       $('.chat-arrow').toggleClass('rotate-arrow');
       $('.chat-row2, .chat-row3, .chat-row4, .chat-row5, .chat-row6').toggleClass('chat-slideup');
     });
--- public/js/basicjs1.js
+++ public/js/basicjs1.js
@@ -4,5 +4,6 @@ $('.svg-hamburger').on('click', function () {
   });
   $('.body1-row7 button').on('click', function () {
     $('.body1-row2-row1-row1-row6 button').click();
-  })
+  });
+  $('.top-head').parallax({imageSrc: '../images/getStudent1.jpg'});
 })
