c1c60fcf70b8112cd27ae88489ea0ce0bb6729b4
rahulraj16mar <31818361+rahulraj16mar@users.noreply.github.com>
2018-09-15 11:45:22 +0530

    modified

--- public/js/basicjs.js
+++ public/js/basicjs.js
@@ -3,11 +3,26 @@ $('.svg-hamburger').on('click', function () {
     $('.navbar-content').toggleClass('navbar-content-slide');
     $('body').toggleClass('stop-scrolling');
   });
-  $('.chat-row1').on('click', function () {
-    $('.chat-row2, .chat-row3, .chat-row4, .chat-row5, .chat-row6').toggleClass('chat-slideup');
-    $('.chat-row1 div').toggleClass('rotate-arrow');
-  });
   $('.body2-row3-button1 button').on('click', function () {
-  $('.body2-row2-extra-chat-row6 button').click();
-})
+    $('.body2-row2-extra-chat-row6 button').click();
+  });
+  if (window.innerWidth < 800) {
+$('.mobile-symbol').on('click', function () {
+      $('.chat-cover').addClass('mobile-symbol-slide');
+      $('.chat').addClass('chat-mobile-symbol-slide');
+      $('.chat-arrow').addClass('rotate-arrow');
+    });
+    $('.chat-row1').on('click', function () {
+      $('.chat-cover').removeClass('mobile-symbol-slide');
+        $('.chat').removeClass('chat-mobile-symbol-slide');
+        $('.chat-arrow').removeClass('rotate-arrow');
+    });
+
+} else if(window.innerWidth > 800) {
+$('.chat-row1').on('click', function () {
+      $('.chat-arrow').toggleClass('rotate-arrow');
+      $('.chat-row2, .chat-row3, .chat-row4, .chat-row5, .chat-row6').toggleClass('chat-slideup');
+    });
+}
+
 })
